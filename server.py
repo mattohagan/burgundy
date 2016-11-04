3b224118a179f0b91770be93c070e1f8e124accd
Matt O'Hagan <mattohagan@Matts-MacBook-Pro-2.local>
2016-11-04 13:33:12 -0400

    add switching between words and prompts

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from bottle import route, hook, response, run, static_file
 import random, optparse
 
 words = tuple(set(map(lambda x:x.lower().strip(), open('words.txt').read().splitlines())))
+prompts = tuple(set(map(lambda x:x.lower().strip(), open('prompts.txt').read().splitlines())))
 
 @hook('after_request')
 def enable_cors():
@@ -12,8 +13,12 @@ def root():
 	return static_file('index.html', 'static')
 
 @route('/word')
-def index():
+def get_word():
     return random.choice(words)
+
+@route('/prompt')
+def get_prompt():
+    return random.choice(prompts)
 
 @route('/static/<filename>')
 def static_files(filename):
--- static/index.js
+++ static/index.js
@@ -1,40 +1,91 @@
 $(document).ready(function() {
 
-    var loaded = [4];
-    var intervalVar;
+    var WORDS_VIEW = 'words';
+    var PROMPTS_VIEW = 'prompts';
+    var currentView = WORDS_VIEW;
+    var promptsUsed = false;
+    var wordsUsed = false;
 
-    function hmmDotting() {
-        if (loaded[0] > 0) {
-            $('#suggestion')[0].textContent = 'hmm' + ['', '.', '..', '...'][4 - loaded[0]];
-            loaded[0] = loaded[0] - 1;
-        } else {
-            clearInterval(intervalVar);
+    function registerTap() {
+        if(currentView == WORDS_VIEW) {
+            changeWord();
+        } else if(currentView == PROMPTS_VIEW) {
+            changePrompt();
         }
     }
 
-    intervalVar = window.setInterval(function() {
-        hmmDotting();
-    }, 300);
-
     function changeWord() {
-        // previoius url http://burgundy.io:8080/
         $.get('/word', function(data) {
-            loaded[0] = 0;
-            $('#suggestion')[0].textContent = data;
-            clearInterval(intervalVar);
+            wordsUsed = true;
+            $('#help-words').addClass('hide');
+            $('#word').removeClass('hide');
+            $('#word')[0].textContent = data;
         });
+    }
+
+    function changePrompt() {
+        $.get('/prompt', function(data) {
+            promptsUsed = true;
+            $('#help-prompts').addClass('hide');
+            $('#prompt').removeClass('hide');
+            $('.prompt-title').removeClass('hide');
+            $('#prompt')[0].textContent = data;
+        });
+    }
+
+    function switchToWords() {
+        if(currentView != WORDS_VIEW) {
+            currentView = WORDS_VIEW;
+            $('#words').toggleClass('active');
+            $('#prompts').toggleClass('active');
+
+            $('#prompt').addClass('hide');
+            $('.prompt-title').addClass('hide');
+            $('#help-prompts').addClass('hide');
+
+            if(wordsUsed) {
+                $('#word').removeClass('hide');
+            } else {
+                $('#help-words').removeClass('hide');
+            }
+            // hide prompts
+            // show words
+        }
+    }
+
+    function switchToPrompts() {
+        if(currentView != PROMPTS_VIEW) {
+            currentView = PROMPTS_VIEW;
+            $('#words').toggleClass('active');
+            $('#prompts').toggleClass('active');
+
+            $('#word').addClass('hide');
+            $('#help-words').addClass('hide');
+
+            if(promptsUsed) {
+                $('#prompt').removeClass('hide');
+                $('.prompt-title').removeClass('hide');
+            } else {
+                $('#help-prompts').removeClass('hide');
+            }
+            // hide prompts
+            // show words
+        }
     }
 
     function adjustFont() {
         windowWidth = parseInt($('html').css('width').replace('px',''));
         if (windowWidth > 850) windowWidth = 850;
-        $('#suggestion').css('font-size', windowWidth/7.5 + "px")
+        $('#word').css('font-size', windowWidth/7.5 + "px")
+        $('#prompt').css('font-size', windowWidth/9 + "px")
     }
 
     adjustFont();
-    changeWord();
 
     $(window).resize(adjustFont);
     $(document).keypress(changeWord);
-    $$('body').tap(changeWord);
+    $$('#suggestion').tap(registerTap);
+
+    $$('#words').tap(switchToWords);
+    $$('#prompts').tap(switchToPrompts);
 });
